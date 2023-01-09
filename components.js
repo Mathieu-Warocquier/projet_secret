6bb71307fa380f1c8cbb7716094d0e680b31abf6
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-01-09 21:06:13 +0100

    variables coleurs css

--- components.js
+++ components.js
@@ -12,6 +12,6 @@ const LIFE_LOST = new Audio("sounds/life_lost.mp3");
 const LEVEL_IMG = new Image();
 LEVEL_IMG.src = "img/level.png";
 const LIFE_IMG = new Image();
-LIFE_IMG.src = "img/life.png";
+LIFE_IMG.src = "img/life1.png";
 const SCORE_IMG = new Image();
 SCORE_IMG.src = "img/score.png";
