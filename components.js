b5d2c0573aaa44eb3a5097bd7264bf907052edc9
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-01-09 11:17:02 +0100

    varaibles js ok

--- components.js
+++ components.js
@@ -5,3 +5,13 @@ const PADDLE_HIT = new Audio("sounds/paddle_hit.mp3");
 const BRICK_HIT = new Audio("sounds/brick_hit.mp3");
 const WIN = new Audio("sounds/win.mp3");
 const LIFE_LOST = new Audio("sounds/life_lost.mp3");
+
+
+// chargement des images
+
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = "img/level.png";
+const LIFE_IMG = new Image();
+LIFE_IMG.src = "img/life.png";
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "img/score.png";
