7f8576a41df4419c37c595ea43aed9a2ec355705
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-01-09 11:11:33 +0100

    variables sonores init

---
+++ components.js
@@ -0,0 +1,7 @@
+// chargement des effets sonores
+
+const WALL_HIT = new Audio("sounds/wall.mp3");
+const PADDLE_HIT = new Audio("sounds/paddle_hit.mp3");
+const BRICK_HIT = new Audio("sounds/brick_hit.mp3");
+const WIN = new Audio("sounds/win.mp3");
+const LIFE_LOST = new Audio("sounds/life_lost.mp3");
