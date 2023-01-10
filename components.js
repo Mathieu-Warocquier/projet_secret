59cfeef88732d73093e549efe6c08a05aeec733a
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-01-10 13:32:55 +0100

    fix des sons

--- components.js
+++ components.js
@@ -4,7 +4,7 @@ const WALL_HIT = new Audio("sounds/wall.mp3");
 const PADDLE_HIT = new Audio("sounds/paddle_hit.mp3");
 const BRICK_HIT = new Audio("sounds/brick_hit.mp3");
 const WIN = new Audio("sounds/win.mp3");
-const LIFE_LOST = new Audio("sounds/life_lost.mp3");
+const LIFE_LOST = new Audio("sounds/life_lost1.mp3");
 
 
 // chargement des images
